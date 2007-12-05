4962029ae2ddd21c62d9c3b49d837cfb9231ce67
arkanes <arkanes@a57810f1-d733-0410-bb68-4527006b89d7>
2007-12-05 23:09:33 +0000

    Some nice tool changes

--- viewer.py
+++ viewer.py
@@ -8,6 +8,8 @@ import wx
 import wx.aui
 from wx.lib.mixins import treemixin
 import svg.document as document
+import pyparsing
+#~ pyparsing.debugging = True
 
 
 
@@ -24,6 +26,20 @@ class ReferencePanel(wx.Panel):
         else:
             dc.DrawText("No image available", 30, 30)
 
+class ProfileResults(wx.TextCtrl):
+    def __init__(self, parent):
+        super(ProfileResults, self).__init__(parent, style=wx.TE_MULTILINE)
+        self.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, face="Consolas"))
+    def SetResults(self, results):
+        if results is None:
+            self.SetValue("")
+            return
+        buf = StringIO()
+        results.stream = buf
+        results.strip_dirs()
+        results.sort_stats(-1)
+        results.print_stats()
+        self.SetValue(buf.getvalue())
 
 
 class XMLTree(wx.TreeCtrl):
@@ -71,7 +87,7 @@ class RenderPanel(wx.PyPanel):
         self.document = document
         self.zoom = 100
         self.offset = wx.Point(0,0)
-        #self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
+        self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         self.Bind(wx.EVT_MOUSEWHEEL, self.OnWheel)
         self.Bind(wx.EVT_MOTION, self.OnMotion)
@@ -81,7 +97,9 @@ class RenderPanel(wx.PyPanel):
         
     def OnPaint(self, evt):
         start = time.time()
-        dc = wx.PaintDC(self)
+        dc = wx.BufferedPaintDC(self)
+        dc.SetBackground(wx.Brush(self.GetBackgroundColour()))
+        dc.Clear()
         if not self.document:
             dc.DrawText("No Document", 20, 20)
             return
@@ -140,8 +158,10 @@ class ViewFrame(wx.Frame):
         self._mgr.SetManagedWindow(self)
         
         self.wrap = wx.Panel(self)
+        self.profileLoading = True
         
         self.tree = XMLTree(self, None)
+        self.profileResults = ProfileResults(self)
         self.render = RenderPanel(self.wrap)
         self.reference = ReferencePanel(self.wrap, None)
         sz = wx.BoxSizer(wx.HORIZONTAL)
@@ -157,10 +177,20 @@ class ViewFrame(wx.Frame):
                 wx.aui.AuiPaneInfo().
                     Top().
                     CloseButton(False).
-                    Layer(0).
+                    Layer(1).
                     Caption("XML Tree").
                     MinSize(self.tree.GetBestSize()),
             "XML TREE"
+        )
+        self._mgr.AddPane(
+            self.profileResults, 
+                wx.aui.AuiPaneInfo().
+                    Top().
+                    CloseButton(False).
+                    Layer(1).
+                    Caption("PROFILE RESULTS").
+                    MinSize(self.tree.GetBestSize()),
+            "PROFILE RESULTS"
         )
         
         self._mgr.AddPane(
@@ -173,16 +203,25 @@ class ViewFrame(wx.Frame):
         self._mgr.Update()
         
         self.Bind(wx.EVT_MENU, self.OnOpenFile, id=wx.ID_OPEN)
+        def OnProfileLoading(evt):
+            self.profileLoading = bool(evt.Checked())
+        self.Bind(wx.EVT_MENU, OnProfileLoading, id=wx.ID_FORWARD)
+        self.Bind(wx.EVT_MENU, self.Reload, id=wx.ID_REFRESH)
         self.Bind(wx.EVT_CHOICE, self.OnChooseFile)
         self.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnTreeSelectionChange)
         self.Bind(wx.EVT_UPDATE_UI, self.OnUpdateUI)
+        
         self.filePicker.SetSelection(self.filePicker.FindString('paths-data-03-f'))
         self.OnChooseFile(None)
         
                 
     def makeMenus(self):
         fileMenu = wx.Menu()
+        mi = wx.MenuItem(fileMenu, wx.ID_FORWARD, "Profile loading", kind=wx.ITEM_CHECK)
+        fileMenu.AppendItem(mi)
         fileMenu.Append(wx.ID_OPEN, "&Open\tCtrl-O")
+        fileMenu.Append(wx.ID_REFRESH, "&Reload Current File\tF5")
+        
         
         mb = wx.MenuBar()
         mb.Append(fileMenu, "&File")
@@ -213,24 +252,38 @@ class ViewFrame(wx.Frame):
         dir = os.path.join(dir, "compliance", "png")
         return dir
             
-        
+    def Reload(self, evt):
+        self.openFile(self.currentFile)        
     
     def openFile(self, filenameOrBuffer):
         start = time.time()
         tree = etree.parse(filenameOrBuffer)
         try:
+            if self.profileLoading:
+                import cProfile
+                p = cProfile.Profile()
+                p.enable()
             self.document = document.SVGDocument(tree.getroot())
+            if self.profileLoading:
+                import pstats
+                p.disable()
+                results = pstats.Stats(p)
+                self.profileResults.SetResults(results)
+            else:
+                self.profileResults.SetResults(None)
+                
             self.render.document = self.document
         except:
             #pdb.set_trace()
             import traceback
             self.render.document = None
             traceback.print_exc()
-            
+        
         amount = time.time() - start
         self.tree.updateTree(tree)
         self.SetStatusText("Loaded in %2f seconds" % amount, self.LOAD_TIME)
         self.SetStatusText(filenameOrBuffer)
+        self.currentFile = filenameOrBuffer
         
         self.Refresh()
     
@@ -259,9 +312,12 @@ class ViewFrame(wx.Frame):
             return
         path = self.document.paths[element]
         print path
+        
     def OnUpdateUI(self, evt):
         if self.render.lastRender is not None:
             self.SetStatusText("Rendered in %2f seconds" % self.render.lastRender, self.RENDER_TIME)
+        if evt.Id == wx.ID_FORWARD:
+            evt.Checked = self.profileLoading
         
 if __name__ == '__main__':
     import psyco
