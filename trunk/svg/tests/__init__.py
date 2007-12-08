f2222e9a771fb88cfeed407d816e432399a1787d
arkanes <arkanes@a57810f1-d733-0410-bb68-4527006b89d7>
2007-12-08 00:06:28 +0000

    Refactor CSS tests into package

--- trunk/svg/tests/__init__.py
+++ trunk/svg/tests/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 
 from svg.tests.test_pathdata import *
 from svg.tests.test_document import *
-from svg.tests.test_css import *
+from svg.tests.css import *
 from svg.tests.test_attributes import *
 
 if __name__ == '__main__':
