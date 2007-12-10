31bbb54b8d35c5d08348226c8a76383df7100940
arkanes <arkanes@a57810f1-d733-0410-bb68-4527006b89d7>
2007-12-10 13:13:06 +0000

    attempt at monkeypatching in elliptical arc support (broken)

--- svg/__init__.py
+++ svg/__init__.py
@@ -1,12 +1,27 @@
 """
-"""
-try:
-    import wxversion
-    #~ wxversion.ensureMinimal('2.9')
-    wxversion.select('3.8-msw-unicode-custom')
-except:
-    pass
 
-def gd():
-    import document
-    return document.SVGDocument(document.document)
+"""
+
+import wx
+
+def AddEllipticalArc(self, x, y, w, h, startAngle, endAngle, clockwise=False):
+    """ Draws an arc of an ellipse within bounding rect (x,y,w,h) 
+    from startArc to endArc (in radians, relative to the horizontal line of the eclipse)"""
+    
+    #implement in terms of AddArc by applying a transformation matrix
+    mtx = wx.GraphicsRenderer_GetDefaultRenderer().CreateMatrix()
+    path = wx.GraphicsRenderer_GetDefaultRenderer().CreatePath()
+    mtx.Translate(x+w/2.0, y+h/2.0)
+    mtx.Scale(w/2.0, y/2.0)
+    
+    path.AddArc(0, 0, 1, startAngle, endAngle, clockwise)
+    path.Transform(mtx)
+    self.AddPath(path)
+    self.MoveToPoint(path.GetCurrentPoint())
+    self.CloseSubpath()
+    
+if not hasattr(wx.GraphicsPath, "AddEllipticalArc"):
+    wx.GraphicsPath.AddEllipticalArc = AddEllipticalArc
+
+del AddEllipticalArc
+    
